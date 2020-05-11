9fd3d37e31e96ea55f38ff09fe8baef5fecf8ed2
TRINIDAD_ARENAS <trinidad.arenas@uvic.cat>
2020-05-11 17:50:42 +0200

    components

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
         const contenido = document.getElementById("contenido");
         contenido.innerHTML = `
             <ul>
-                <li>Nombre: Trini</li>
+                <li>Components: Dalila Allaoui Ouadi
+                Nicole Cabrera Villavicencio 
+                Tarandeep Sigh Mandahar 
+                Manuel Gregorio Collazo
+                Trinidad Arenas Costa</li>
                 <li>Email: trinidad.arenas@uvic.cat</li>
             </ul>
         `
